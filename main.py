dc45e8ff508fae7418ec0f1f0d1a41247830b4ab
neslav <98667148+neslav@users.noreply.github.com>
2023-04-17 01:38:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+item=1
+name="Slava"
+print(type(item))
+
+result=(name)
+
+print(result)
+
+items=[item, name, 'Alex', 33, [11, 22, 'Hello'], {'age':33}, True, False, {0,1,3}, 4.5]
+
+print(items)
+
+dict_items={'age':33,
+            'name':'Vadim',
+            'location': {'country':'Turkey',
+                        'City':'Antalia'}}
+print(type(dict_items), dict_items['location']['City'])
