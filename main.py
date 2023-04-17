308041ead0a6f9b36ff108c8b320fd80bdaded34
Vyacheslav Yurchenko <v_yurchenko@lesta.group>
2023-04-17 19:37:00 +0300

    1

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
-item=1
-name="Slava"
-print(type(item))
+# item=1
+# name="Slava"
+# print(type(item))
+#
+# result=(name)
+#
+# print(result)
+#
+# items=[item, name, 'Alex', 33, [11, 22, 'Hello'], {'age':33}, True, False, {0,1,3}, 4.5]
+#
+# print(items)
+#
+# dict_items={'age':33,
+#             'name':'Vadim',
+#             'location': {'country':'Turkey',
+#                         'City':'Antalia'}}
+# print(type(dict_items), dict_items['location']['City'])
+#
+#
+#
+# a = input('ведитеслово')
+# start=int(input(6))
+# end=int(input(2))
 
-result=(name)
+# pyta = input(input('please input'))
+# start = int(input('start'))
+# end = int(input('end'))
+# substring = ''
+# i = min(start,end)
+# while i <= max(start,end):
+#     if i < len(a):
+#         substring = substring + a[i]
+#     i = i + 1
+# print (substring)
 
-print(result)
 
-items=[item, name, 'Alex', 33, [11, 22, 'Hello'], {'age':33}, True, False, {0,1,3}, 4.5]
 
-print(items)
+# L=700
+# v_up = 50
+# v_down = 70
+# t = 120
+# t_ost = t % (L/v_up+L/v_down)
+# t_up = L/v_up
+# if t_ost < t_up:
+# res = t_ost*v_up
+# else:
+# path = (t_ost - t_up)*v_down
+# res = L - path
+# print (res)
+#
+# sequence
 
-dict_items={'age':33,
-            'name':'Vadim',
-            'location': {'country':'Turkey',
-                        'City':'Antalia'}}
-print(type(dict_items), dict_items['location']['City'])
+sli = slice(2, 1500, )
+print(sli)
 
-City
+s1='Котопульта'
+s2='пульт'
+i=0
+while i<
