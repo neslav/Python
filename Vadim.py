10e2ddcb976b3093fd893a7810d3448e65cb8e77
neslav <www.slava.by@yandex.by>
2023-04-19 01:53:47 +0300

    1

---
+++ Vadim.py
@@ -0,0 +1,53 @@
+# item=1
+# name="Slava"
+# print(type(item))
+#
+# result=(name)
+#
+# print(result)
+#
+# items=[item, name, 'Alex', 33, [11, 22, 'Hello'], {'age':33}, True, False, {0,1,3}, 4.5]
+#
+# print(items)
+#
+# dict_items={'age':33,
+#             'name':'Vadim',
+#             'location': {'country':'Turkey',
+#                         'City':'Antalia'}}
+# print(type(dict_items), dict_items['location']['City'])
+
+# Python. HW_1.
+
+# 1) Создать переменную типа String
+str='string'
+
+# 2) Создать переменную типа Integer
+int=1
+
+# 3) Создать переменную типа Float
+float=1.5
+
+# 4) Создать переменную типа Bytes
+
+
+# 5) Создать переменную типа List
+list=[1, 'string', 1.5]
+
+# 6) Создать переменную типа Tuple
+tup=(7, 'teen', 234.3)
+
+# 7) Создать переменную типа Set
+set=(7, 'teen', 234.3)
+
+# 8. Создать переменную типа Frozen set
+
+
+# 9) Создать переменную типа Dict
+dict={'name':'Slava', 'age':22}
+
+# 10) Вывести в консоль все выше перечисленные переменные с добавлением типа данных.
+print(type())
+# print(int)
+# 11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные. Вывести в консоль.
+# 12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
+# 13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
